9299b82e0142e16759bcf55ef705c12eccdcea3b
vicjose007 <72372763+vicjose007@users.noreply.github.com>
2021-11-29 21:38:21 -0400

    Add files via upload

---
+++ TestDoor2.cs
@@ -0,0 +1,48 @@
+using ISBNc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xunit;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace unitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [Fact]
+        public void OpenDoor()
+        {
+            Door obj = new Door();
+
+            string key = "Open";
+
+            bool result = obj.OpenDoor(key);
+
+            Assert.IsTrue(result);
+
+        }
+        [Fact]
+        public void CloseDoor()
+        {
+            Door obj = new Door();
+
+            string key = "Close";
+
+            bool result = obj.CloseDoor(key);
+
+            Assert.IsFalse(result);
+
+        }
+        [Fact]
+        public void CloseDoorLoked()
+        {
+            Door obj = new Door();
+
+            string key = "Loked";
+
+            bool result = obj.LockedDoor(key);
+
+            Assert.IsTrue(result);
+
+        }
+    }
+}
